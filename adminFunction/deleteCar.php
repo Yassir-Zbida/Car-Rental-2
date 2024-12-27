60ded2f4114831afbb067a792a02d429573ebb6f
Safaa-Ettalhi <safaeettalhi@gmail.com>
2024-12-27 16:31:51 +0100

    ajout deleteCar qui permet de supprimer les car

--- adminFunction/deleteCar.php
+++ adminFunction/deleteCar.php
@@ -1,37 +1,22 @@
 <?php
 require_once("../db.php");
- 
+require_once("../pages/car.php");
     
-
-class Car {
-    private $connection;
-
-    public function __construct($dbConnection) {
-        $this->connection = $dbConnection;
-    }
-
-    public function deleteCar($id) {
-        $stmt = $this->connection->prepare("DELETE FROM cars WHERE ID = ?");
-        $stmt->bind_param("i", $id);
-        if ($stmt->execute()) {
-            header("Location: /pages/cars.php");
-            exit();
-        } else {
-            echo "Erreur : " . $stmt->error;
-        }
-        $stmt->close();
-    }
-}
 
 if (isset($_GET["id"])) {
     $id = intval($_GET["id"]);
-
-    // Connexion à la base de données
     $db = new Database();
-    $connection = $db->getConnection();
-
-    // Gestion des voitures
-    $car = new Car($connection);
-    $car->deleteCar($id);
+    $car = new Car($db);
+   $result= $car->deleteCar($id);
+    
+   if ($result) { 
+    header("Location: ../pages/cars.php?success=deleted");
+    exit();
+} else {
+    echo "Erreur lors de la suppression de la voiture.";
 }
+} else {
+    echo "ID de car manquant.";
+}
+
 ?>
